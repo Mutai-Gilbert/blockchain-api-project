b8906cf6b7e655a646b4f9e6abf73ab49ae0c4f8
mutai-gilbert <mutaigilbert85@gmail.com>
2023-12-12 21:28:28 +0300

    Add new changes to index.js

--- index.js
+++ index.js
@@ -3,17 +3,23 @@ import axios from "axios"
 
 const app = express();
 const port = process.env.PORT || 3000;
+const URL = "https://api.blockchain.com/v3/exchange";
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', async(req, res) => {
     try{
-        const response = await axios.get('');
-        const blockChainData = response.data;
+        const response = await axios.get(URL);
+        const blockChainData = JSON.stringify(response);
+        res.render("index", {
+            blockChainData
+        });
     } catch(error) {
         console.error('Error fetching data from the blockchain API:', error.message);
-        res.status(500).send('Internal Server Error');
+        res.status(500).render('error', {
+            errorMessage: "Internal Server Error"
+        });
     }
 });
 
